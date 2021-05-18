39b05fa559a5df7a5a4e106b86d9dc1058bd6ea2
jameswilddev <jameswilddev@outlook.com>
2021-05-18 20:20:55 +0100

    Add logger for parsing events.

---
+++ src/Parsing/ParserTarget.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace JamesWildDev\DBMLParser\Parsing;
+
+/**
+ * Handles declarations found by the Parser.
+ */
+interface ParserTarget
+{
+  /**
+   * Handle the declaration of a table.
+   *
+   * @param string $name The name of the table.
+   * @param integer $nameStartLine The line number on which the name of the table started.
+   * @param integer $nameStartColumn The column number on which the name of the table started.
+   * @param integer $nameEndLine The line number on which the name of the table ended.
+   * @param integer $nameEndColumn The column number on which the name of the table ended.
+   */
+  public function table($name, $nameStartLine, $nameStartColumn, $nameEndLine, $nameEndColumn);
+
+  /**
+   * Handle the declaration of a table alias.
+   *
+   * @param string $tableName The name of the parent table.
+   * @param string $name The name of the table alias.
+   * @param integer $nameStartLine The line number on which the name of the table alias started.
+   * @param integer $nameStartColumn The column number on which the name of the table alias started.
+   * @param integer $nameEndLine The line number on which the name of the table alias ended.
+   * @param integer $nameEndColumn The column number on which the name of the table alias ended.
+   */
+  public function tableAlias($tableName, $name, $nameStartLine, $nameStartColumn, $nameEndLine, $nameEndColumn);
+
+  /**
+   * Handle the declaration of a table note.
+   *
+   * @param string $tableName The name of the table to which a note is to be added.
+   * @param string $content The content of the note to be added.
+   * @param integer $contentStartLine The line number on which the note started.
+   * @param integer $contentStartColumn The column number on which the note started.
+   * @param integer $contentEndLine The line number on which the note ended.
+   * @param integer $contentEndColumn The column number on which the note ended.
+   */
+  public function tableNote($tableName, $content, $contentStartLine, $contentStartColumn, $contentEndLine, $contentEndColumn);
+
+  /**
+   * Handle the declaration of a column.
+   *
+   * @param string $tableName The name of the parent table.
+   * @param string $name The name of the column.
+   * @param integer $nameStartLine The line number on which the name of the column started.
+   * @param integer $nameStartColumn The column number on which the name of the column started.
+   * @param integer $nameEndLine The line number on which the name of the column ended.
+   * @param integer $nameEndColumn The column number on which the name of the column ended.
+   * @param string $type The column's type.
+   * @param ?string $size The column's size, if specified, otherwise, null.
+   */
+  public function column($tableName, $name, $nameStartLine, $nameStartColumn, $nameEndLine, $nameEndColumn, $type, $size);
+
+  /**
+   * Handle the declaration of a column note.
+   *
+   * @param string $tableName The name of the table which contains the column to which a note is to be added.
+   * @param string $columnName The name of the column to which a note is to be added.
+   * @param string $content The content of the note to be added.
+   * @param integer $contentStartLine The line number on which the note started.
+   * @param integer $contentStartColumn The column number on which the note started.
+   * @param integer $contentEndLine The line number on which the note ended.
+   * @param integer $contentEndColumn The column number on which the note ended.
+   */
+  public function columnNote($tableName, $columnName, $content, $contentStartLine, $contentStartColumn, $contentEndLine, $contentEndColumn);
+
+  /**
+   * Handle the declaration that a column is a primary key.
+   *
+   * @param string $tableName The name of the table which contains the column which is a primary key.
+   * @param string $columnName The name of the column which is a primary key.
+   */
+  public function columnPrimaryKey($tableName, $columnName);
+
+  /**
+   * Handle the declaration that a column is auto-incrementing.
+   *
+   * @param string $tableName The name of the table which contains the column which is auto-incrementing.
+   * @param string $columnName The name of the column which is auto-incrementing.
+   */
+  public function columnIncrement($tableName, $columnName);
+
+  /**
+   * Handle the declaration that a column is not nullable.
+   *
+   * @param string $tableName The name of the table which contains the column which is not nullable.
+   * @param string $columnName The name of the column which is not nullable.
+   */
+  public function columnNotNull($tableName, $columnName);
+
+  /**
+   * Handle the declaration that a column is to have a constant default value.
+   *
+   * @param string $tableName The name of the table which contains the column which is to have a constant default value.
+   * @param string $columnName The name of the column which is to have a constant default value.
+   * @param string $content The default value.
+   * @param integer $contentStartLine The line number on which the default value started.
+   * @param integer $contentStartColumn The column number on which the default value started.
+   * @param integer $contentEndLine The line number on which the default value ended.
+   * @param integer $contentEndColumn The column number on which the default value ended.
+   */
+  public function columnConstantDefault($tableName, $columnName, $content, $contentStartLine, $contentStartColumn, $contentEndLine, $contentEndColumn);
+
+  /**
+   * Handle the declaration that a column is to have a calculated default value.
+   *
+   * @param string $tableName The name of the table which contains the column which is to have a calculated default value.
+   * @param string $columnName The name of the column which is to have a calculated default value.
+   * @param string $content The query used to calculate the default value.
+   * @param integer $contentStartLine The line number on which the query used to calculate the default value started.
+   * @param integer $contentStartColumn The column number on which the query used to calculate the default value started.
+   * @param integer $contentEndLine The line number on which the query used to calculate the default value ended.
+   * @param integer $contentEndColumn The column number on which the query used to calculate the default value ended.
+   */
+  public function columnCalculatedDefault($tableName, $columnName, $content, $contentStartLine, $contentStartColumn, $contentEndLine, $contentEndColumn);
+
+  /**
+   * Handle the declaration of a foreign key constraint.
+   *
+   * @param string $referencingTableNameOrAlias The name or an alias of the table which contains the referencing column.
+   * @param integer $referencingTableNameOrAliasStartLine The line number on which the name or an alias of the table which contains the referencing column started.
+   * @param integer $referencingTableNameOrAliasStartColumn The column number on which the name or an alias of the table which contains the referencing column started.
+   * @param integer $referencingTableNameOrAliasEndLine The line number on which the name or an alias of the table which contains the referencing column ended.
+   * @param integer $referencingTableNameOrAliasEndColumn The column number on which the name or an alias of the table which contains the referencing column ended.
+   * @param string $referencingColumnName The name of the column which contains references.
+   * @param integer $referencingColumnNameStartLine The line number on which the column which contains references started.
+   * @param integer $referencingColumnNameStartColumn The column number on which the column which contains references started.
+   * @param integer $referencingColumnNameEndLine The line number on which the column which contains references ended.
+   * @param integer $referencingColumnNameEndColumn The column number on which the column which contains references ended.
+   * @param string $referencedTableNameOrAlias The name or an alias of the table which contains the column to be referred to.
+   * @param integer $referencedTableNameOrAliasStartLine The line number on which the name or an alias of the table which contains the column to be referred to started.
+   * @param integer $referencedTableNameOrAliasStartColumn The column number on which the name or an alias of the table which contains the column to be referred to started.
+   * @param integer $referencedTableNameOrAliasEndLine The line number on which the name or an alias of the table which contains the column to be referred to ended.
+   * @param integer $referencedTableNameOrAliasEndColumn The column number on which the name or an alias of the table which contains the column to be referred to ended.
+   * @param string $referencedColumnName The name of the column which contains values to be referred to.
+   * @param integer $referencedColumnNameStartLine The line number on which the name of the column which contains values to be referred to started.
+   * @param integer $referencedColumnNameStartColumn The column number on which the name of the column which contains values to be referred to started.
+   * @param integer $referencedColumnNameEndLine The line number on which the name of the column which contains values to be referred to ended.
+   * @param integer $referencedColumnNameEndColumn The column number on which the name of the column which contains values to be referred to ended.
+   */
+  public function foreignKeyConstraint(
+    $referencingTableNameOrAlias,
+    $referencingTableNameOrAliasStartLine,
+    $referencingTableNameOrAliasStartColumn,
+    $referencingTableNameOrAliasEndLine,
+    $referencingTableNameOrAliasEndColumn,
+    $referencingColumnName,
+    $referencingColumnNameStartLine,
+    $referencingColumnNameStartColumn,
+    $referencingColumnNameEndLine,
+    $referencingColumnNameEndColumn,
+    $referencedTableNameOrAlias,
+    $referencedTableNameOrAliasStartLine,
+    $referencedTableNameOrAliasStartColumn,
+    $referencedTableNameOrAliasEndLine,
+    $referencedTableNameOrAliasEndColumn,
+    $referencedColumnName,
+    $referencedColumnNameStartLine,
+    $referencedColumnNameStartColumn,
+    $referencedColumnNameEndLine,
+    $referencedColumnNameEndColumn
+  );
+
+  /**
+   * Handle the declaration that one or more columns within a table are to have an index.
+   *
+   * @param string $tableName The name of the table which contains the columns which are to have an index.
+   * @param string $columns An array of associative arrays where:
+   *                        - name => The name of the column (string).
+   *                        - nameStartLine => The line number on which the name of the column started.
+   *                        - nameStartColumn => The line number on which the name of the column started.
+   *                        - nameEndLine => The line number on which the name of the column ended.
+   *                        - nameEndColumn => The line number on which the name of the column ended.
+   * @param ?string $name The name of the index, if any, otherwise, null.
+   * @param ?integer $nameStartLine The line number on which the name of the index started, if any, otherwise, null.
+   * @param ?integer $nameStartColumn The column number on which the name of the index started, if any, otherwise, null.
+   * @param ?integer $nameEndLine The line number on which the name of the index ended, if any, otherwise, null.
+   * @param ?integer $nameEndColumn The column number on which the name of the index ended, if any, otherwise, null.
+   * @param boolean $unique When true, duplicate sets of the values are disallowed.  When false, they are allowed.
+   */
+  public function index($tableName, $columns, $name, $nameStartLine, $nameStartColumn, $nameEndLine, $nameEndColumn, $unique);
+
+  /**
+   * Handle the declaration of an enum.
+   *
+   * @param string $name The name of the enum.
+   * @param integer $nameStartLine The line number on which the name of the enum started.
+   * @param integer $nameStartColumn The column number on which the name of the enum started.
+   * @param integer $nameEndLine The line number on which the name of the enum ended.
+   * @param integer $nameEndColumn The column number on which the name of the enum ended.
+   */
+  public function enum($name, $nameStartLine, $nameStartColumn, $nameEndLine, $nameEndColumn);
+
+  /**
+   * Handle the declaration of a value of an enum.
+   *
+   * @param string $enumName The name of the parent enum.
+   * @param string $name The name of the value.
+   * @param integer $nameStartLine The line number on which the name of the value started.
+   * @param integer $nameStartColumn The column number on which the name of the value started.
+   * @param integer $nameEndLine The line number on which the name of the value ended.
+   * @param integer $nameEndColumn The column number on which the name of the value ended.
+   */
+  public function enumValue($enumName, $name, $nameStartLine, $nameStartColumn, $nameEndLine, $nameEndColumn);
+
+  /**
+   * Handle the declaration of an enum value note.
+   *
+   * @param string $enumName The name of the enum which contains the value to which a note is to be added.
+   * @param string $name The name of the value to which a note is to be added.
+   * @param string $content The content of the note to be added.
+   * @param integer $contentStartLine The line number on which the note started.
+   * @param integer $contentStartColumn The column number on which the note started.
+   * @param integer $contentEndLine The line number on which the note ended.
+   * @param integer $contentEndColumn The column number on which the note ended.
+   */
+  public function enumValueNote($enumName, $name, $content, $contentStartLine, $contentStartColumn, $contentEndLine, $contentEndColumn);
+}
---
+++ tests/Unit/Parsing/Logging/LogParserTargetTest.php
@@ -0,0 +1,340 @@
+<?php
+
+namespace JamesWildDev\DBMLParser\Tests\Unit\Parsing\Logging;
+
+use JamesWildDev\DBMLParser\Parsing\Logging\ColumnCalculatedDefaultEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\ColumnConstantDefaultEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\ColumnEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\ColumnIncrementEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\ColumnNoteEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\ColumnNotNullEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\ColumnPrimaryKeyEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\EnumEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\EnumValueEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\EnumValueNoteEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\ForeignKeyConstraintEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\IndexEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\LogParserTarget;
+use JamesWildDev\DBMLParser\Parsing\Logging\TableAliasEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\TableEvent;
+use JamesWildDev\DBMLParser\Parsing\Logging\TableNoteEvent;
+use PHPUnit\Framework\TestCase;
+
+final class LogParserTargetTest extends TestCase
+{
+  public function test_initially_empty()
+  {
+    $logParserTarget = new LogParserTarget();
+
+    $this->assertEmpty($logParserTarget->events);
+  }
+
+  public function test_logs_all_events()
+  {
+    $logParserTarget = new LogParserTarget();
+
+    $logParserTarget->columnNote(
+      'Test Column Note Table Name',
+      'Test Column Note Column Name',
+      'Test Column Note Content',
+      77,
+      42,
+      932,
+      21
+    );
+    $logParserTarget->columnPrimaryKey(
+      'Test Column Primary Key Table Name',
+      'Test Column Primary Key Column Name'
+    );
+    $logParserTarget->columnIncrement(
+      'Test Column Increment Table Name',
+      'Test Column Increment Column Name'
+    );
+    $logParserTarget->enum(
+      'Test Enum Name',
+      62,
+      78,
+      94,
+      268
+    );
+    $logParserTarget->enumValue(
+      'Test Enum Value Enum Name',
+      'Test Enum Value Name',
+      47,
+      28,
+      79,
+      58
+    );
+    $logParserTarget->enumValueNote(
+      'Test Enum Value Note Enum Name',
+      'Test Enum Value Note Name',
+      'Test Enum Value Note Content',
+      72,
+      52,
+      95,
+      52
+    );
+    $logParserTarget->columnNotNull(
+      'Test Column Not Null Table Name',
+      'Test Column Not Null Column Name'
+    );
+    $logParserTarget->columnConstantDefault(
+      'Test Column Constant Default Table Name',
+      'Test Column Constant Default Column Name',
+      'Test Column Constant Default Content',
+      883,
+      21,
+      45,
+      78
+    );
+    $logParserTarget->columnCalculatedDefault(
+      'Test Column Calculated Default Event Table Name',
+      'Test Column Calculated Default Event Column Name',
+      'Test Column Calculated Default Event Content',
+      222,
+      48,
+      52,
+      10
+    );
+    $logParserTarget->foreignKeyConstraint(
+      'Test Foreign Key Constraint Referencing Table Name Or Alias',
+      883,
+      632,
+      644,
+      512,
+      'Test Foreign Key Constraint Referencing Column Name',
+      38,
+      92,
+      25,
+      70,
+      'Test Foreign Key Constraint Referenced Table Name Or Alias',
+      104,
+      160,
+      80,
+      98,
+      'Test Foreign Key Constraint Referenced Column Name',
+      401,
+      5022,
+      46,
+      70
+    );
+    $logParserTarget->index(
+      'Test Index Table Name',
+      [
+        [
+          'name' => 'Test Index Column A Name',
+          'nameStartLine' => 72,
+          'nameStartColumn' => 14,
+          'nameEndLine' => 64,
+          'nameEndColumn' => 55,
+        ],
+        [
+          'name' => 'Test Index Column B Name',
+          'nameStartLine' => 26,
+          'nameStartColumn' => 85,
+          'nameEndLine' => 71,
+          'nameEndColumn' => 22,
+        ],
+        [
+          'name' => 'Test Index Column C Name',
+          'nameStartLine' => 46,
+          'nameStartColumn' => 58,
+          'nameEndLine' => 99,
+          'nameEndColumn' => 86,
+        ],
+      ],
+      'Test Index Name',
+      653,
+      232,
+      396,
+      942,
+      true
+    );
+    $logParserTarget->table(
+      'Test Table Name',
+      38,
+      95,
+      46,
+      55
+    );
+    $logParserTarget->tableAlias(
+      'Test Table Alias Table Name',
+      'Test Table Alias Name',
+      31,
+      26,
+      11,
+      87
+    );
+    $logParserTarget->tableNote(
+      'Test Table Note Table Name',
+      'Test Table Note Content',
+      65,
+      82,
+      44,
+      12
+    );
+    $logParserTarget->column(
+      'Test Column Table Name',
+      'Test Column Name',
+      48,
+      99,
+      105,
+      75,
+      'Test Column Type',
+      'Test Column Size'
+    );
+
+    $this->assertEquals([
+      new ColumnNoteEvent(
+        'Test Column Note Table Name',
+        'Test Column Note Column Name',
+        'Test Column Note Content',
+        77,
+        42,
+        932,
+        21
+      ),
+      new ColumnPrimaryKeyEvent(
+        'Test Column Primary Key Table Name',
+        'Test Column Primary Key Column Name'
+      ),
+      new ColumnIncrementEvent(
+        'Test Column Increment Table Name',
+        'Test Column Increment Column Name'
+      ),
+      new EnumEvent(
+        'Test Enum Name',
+        62,
+        78,
+        94,
+        268
+      ),
+      new EnumValueEvent(
+        'Test Enum Value Enum Name',
+        'Test Enum Value Name',
+        47,
+        28,
+        79,
+        58
+      ),
+      new EnumValueNoteEvent(
+        'Test Enum Value Note Enum Name',
+        'Test Enum Value Note Name',
+        'Test Enum Value Note Content',
+        72,
+        52,
+        95,
+        52
+      ),
+      new ColumnNotNullEvent(
+        'Test Column Not Null Table Name',
+        'Test Column Not Null Column Name'
+      ),
+      new ColumnConstantDefaultEvent(
+        'Test Column Constant Default Table Name',
+        'Test Column Constant Default Column Name',
+        'Test Column Constant Default Content',
+        883,
+        21,
+        45,
+        78
+      ),
+      new ColumnCalculatedDefaultEvent(
+        'Test Column Calculated Default Event Table Name',
+        'Test Column Calculated Default Event Column Name',
+        'Test Column Calculated Default Event Content',
+        222,
+        48,
+        52,
+        10
+      ),
+      new ForeignKeyConstraintEvent(
+        'Test Foreign Key Constraint Referencing Table Name Or Alias',
+        883,
+        632,
+        644,
+        512,
+        'Test Foreign Key Constraint Referencing Column Name',
+        38,
+        92,
+        25,
+        70,
+        'Test Foreign Key Constraint Referenced Table Name Or Alias',
+        104,
+        160,
+        80,
+        98,
+        'Test Foreign Key Constraint Referenced Column Name',
+        401,
+        5022,
+        46,
+        70
+      ),
+      new IndexEvent(
+        'Test Index Table Name',
+        [
+          [
+            'name' => 'Test Index Column A Name',
+            'nameStartLine' => 72,
+            'nameStartColumn' => 14,
+            'nameEndLine' => 64,
+            'nameEndColumn' => 55,
+          ],
+          [
+            'name' => 'Test Index Column B Name',
+            'nameStartLine' => 26,
+            'nameStartColumn' => 85,
+            'nameEndLine' => 71,
+            'nameEndColumn' => 22,
+          ],
+          [
+            'name' => 'Test Index Column C Name',
+            'nameStartLine' => 46,
+            'nameStartColumn' => 58,
+            'nameEndLine' => 99,
+            'nameEndColumn' => 86,
+          ],
+        ],
+        'Test Index Name',
+        653,
+        232,
+        396,
+        942,
+        true
+      ),
+      new TableEvent(
+        'Test Table Name',
+        38,
+        95,
+        46,
+        55
+      ),
+      new TableAliasEvent(
+        'Test Table Alias Table Name',
+        'Test Table Alias Name',
+        31,
+        26,
+        11,
+        87
+      ),
+      new TableNoteEvent(
+        'Test Table Note Table Name',
+        'Test Table Note Content',
+        65,
+        82,
+        44,
+        12
+      ),
+      new ColumnEvent(
+        'Test Column Table Name',
+        'Test Column Name',
+        48,
+        99,
+        105,
+        75,
+        'Test Column Type',
+        'Test Column Size'
+      ),
+    ], $logParserTarget->events);
+  }
+}
