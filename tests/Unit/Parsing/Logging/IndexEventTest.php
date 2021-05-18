c0842bff65e1da018aaea51ee16bee60e83c4cbe
jameswilddev <jameswilddev@outlook.com>
2021-05-18 20:16:41 +0100

    Add missing tests.

--- tests/Unit/Parsing/Logging/IndexEventTest.php
+++ tests/Unit/Parsing/Logging/IndexEventTest.php
@@ -7,6 +7,73 @@ use PHPUnit\Framework\TestCase;
 
 final class IndexEventTest extends TestCase
 {
+  public function test_without_name_non_unique()
+  {
+    $indexEvent = new IndexEvent(
+      'Test Table Name',
+      [
+        [
+          'name' => 'Test Column A Name',
+          'nameStartLine' => 72,
+          'nameStartColumn' => 14,
+          'nameEndLine' => 64,
+          'nameEndColumn' => 55,
+        ],
+        [
+          'name' => 'Test Column B Name',
+          'nameStartLine' => 26,
+          'nameStartColumn' => 85,
+          'nameEndLine' => 71,
+          'nameEndColumn' => 22,
+        ],
+        [
+          'name' => 'Test Column C Name',
+          'nameStartLine' => 46,
+          'nameStartColumn' => 58,
+          'nameEndLine' => 99,
+          'nameEndColumn' => 86,
+        ],
+      ],
+      null,
+      null,
+      null,
+      null,
+      null,
+      false
+    );
+
+    $this->assertEquals('Test Table Name', $indexEvent->tableName);
+    $this->assertEquals([
+      [
+        'name' => 'Test Column A Name',
+        'nameStartLine' => 72,
+        'nameStartColumn' => 14,
+        'nameEndLine' => 64,
+        'nameEndColumn' => 55,
+      ],
+      [
+        'name' => 'Test Column B Name',
+        'nameStartLine' => 26,
+        'nameStartColumn' => 85,
+        'nameEndLine' => 71,
+        'nameEndColumn' => 22,
+      ],
+      [
+        'name' => 'Test Column C Name',
+        'nameStartLine' => 46,
+        'nameStartColumn' => 58,
+        'nameEndLine' => 99,
+        'nameEndColumn' => 86,
+      ],
+    ], $indexEvent->columns);
+    $this->assertNull($indexEvent->name);
+    $this->assertNull($indexEvent->nameStartLine);
+    $this->assertNull($indexEvent->nameStartColumn);
+    $this->assertNull($indexEvent->nameEndLine);
+    $this->assertNull($indexEvent->nameEndColumn);
+    $this->assertFalse($indexEvent->unique);
+  }
+
   public function test_with_name_non_unique()
   {
     $indexEvent = new IndexEvent(
@@ -72,5 +139,139 @@ final class IndexEventTest extends TestCase
     $this->assertEquals(64, $indexEvent->nameEndLine);
     $this->assertEquals(17, $indexEvent->nameEndColumn);
     $this->assertFalse($indexEvent->unique);
+  }
+
+  public function test_without_name_unique()
+  {
+    $indexEvent = new IndexEvent(
+      'Test Table Name',
+      [
+        [
+          'name' => 'Test Column A Name',
+          'nameStartLine' => 72,
+          'nameStartColumn' => 14,
+          'nameEndLine' => 64,
+          'nameEndColumn' => 55,
+        ],
+        [
+          'name' => 'Test Column B Name',
+          'nameStartLine' => 26,
+          'nameStartColumn' => 85,
+          'nameEndLine' => 71,
+          'nameEndColumn' => 22,
+        ],
+        [
+          'name' => 'Test Column C Name',
+          'nameStartLine' => 46,
+          'nameStartColumn' => 58,
+          'nameEndLine' => 99,
+          'nameEndColumn' => 86,
+        ],
+      ],
+      null,
+      null,
+      null,
+      null,
+      null,
+      true
+    );
+
+    $this->assertEquals('Test Table Name', $indexEvent->tableName);
+    $this->assertEquals([
+      [
+        'name' => 'Test Column A Name',
+        'nameStartLine' => 72,
+        'nameStartColumn' => 14,
+        'nameEndLine' => 64,
+        'nameEndColumn' => 55,
+      ],
+      [
+        'name' => 'Test Column B Name',
+        'nameStartLine' => 26,
+        'nameStartColumn' => 85,
+        'nameEndLine' => 71,
+        'nameEndColumn' => 22,
+      ],
+      [
+        'name' => 'Test Column C Name',
+        'nameStartLine' => 46,
+        'nameStartColumn' => 58,
+        'nameEndLine' => 99,
+        'nameEndColumn' => 86,
+      ],
+    ], $indexEvent->columns);
+    $this->assertNull($indexEvent->name);
+    $this->assertNull($indexEvent->nameStartLine);
+    $this->assertNull($indexEvent->nameStartColumn);
+    $this->assertNull($indexEvent->nameEndLine);
+    $this->assertNull($indexEvent->nameEndColumn);
+    $this->assertTrue($indexEvent->unique);
+  }
+
+  public function test_with_name_unique()
+  {
+    $indexEvent = new IndexEvent(
+      'Test Table Name',
+      [
+        [
+          'name' => 'Test Column A Name',
+          'nameStartLine' => 72,
+          'nameStartColumn' => 14,
+          'nameEndLine' => 64,
+          'nameEndColumn' => 55,
+        ],
+        [
+          'name' => 'Test Column B Name',
+          'nameStartLine' => 26,
+          'nameStartColumn' => 85,
+          'nameEndLine' => 71,
+          'nameEndColumn' => 22,
+        ],
+        [
+          'name' => 'Test Column C Name',
+          'nameStartLine' => 46,
+          'nameStartColumn' => 58,
+          'nameEndLine' => 99,
+          'nameEndColumn' => 86,
+        ],
+      ],
+      'Test Name',
+      12,
+      31,
+      64,
+      17,
+      true
+    );
+
+    $this->assertEquals('Test Table Name', $indexEvent->tableName);
+    $this->assertEquals([
+      [
+        'name' => 'Test Column A Name',
+        'nameStartLine' => 72,
+        'nameStartColumn' => 14,
+        'nameEndLine' => 64,
+        'nameEndColumn' => 55,
+      ],
+      [
+        'name' => 'Test Column B Name',
+        'nameStartLine' => 26,
+        'nameStartColumn' => 85,
+        'nameEndLine' => 71,
+        'nameEndColumn' => 22,
+      ],
+      [
+        'name' => 'Test Column C Name',
+        'nameStartLine' => 46,
+        'nameStartColumn' => 58,
+        'nameEndLine' => 99,
+        'nameEndColumn' => 86,
+      ],
+    ], $indexEvent->columns);
+    $this->assertEquals('Test Name', $indexEvent->name);
+    $this->assertEquals(12, $indexEvent->nameStartLine);
+    $this->assertEquals(31, $indexEvent->nameStartColumn);
+    $this->assertEquals(64, $indexEvent->nameEndLine);
+    $this->assertEquals(17, $indexEvent->nameEndColumn);
+    $this->assertTrue($indexEvent->unique);
   }
 }
